6a800a5a553c48ca71955adebc9ced4de0235326
brandon-rowe <brandon@cyntax.org>
2019-03-04 14:52:13 -0500

    simulation

---
+++ CSCI I, II, III/Sim.java
@@ -0,0 +1,26 @@
+//simulation
+//testing hypothesis for results of game. 
+//given 3 boxes with 2 balls each inside
+//box 1 contains 2 gold balls
+//box 2 contains 2 silver balls
+//box 3 contains 1 gold ball & 1 silver ball
+
+public class Sim
+{
+   public static void main(String[] args)
+   {
+      /*int[] box1 = new int[] {1,1};
+      int[] box2 = new int[] {0,0};
+      int[] box3 = new int[] {0,1};
+      int[] boxes = new int[] {1,1},{0,0},{0,1};*/
+      String input = "000111"
+
+      System.out.println("User picks a box with 1 gold value.");
+      for (int i=0; i<input.Length; i++)
+      {
+         
+      }
+      
+   }
+   
+}
