b054649b1d7e685233fb338e4297e2d50825967c
brandon-rowe <brandon@cyntax.org>
2019-03-04 15:23:04 -0500

    updated sim with array

--- CSCI I, II, III/Sim.java
+++ CSCI I, II, III/Sim.java
@@ -4,23 +4,40 @@
 //box 1 contains 2 gold balls
 //box 2 contains 2 silver balls
 //box 3 contains 1 gold ball & 1 silver ball
+import java.util.*;
+import java.util.Random;
 
 public class Sim
 {
    public static void main(String[] args)
    {
-      /*int[] box1 = new int[] {1,1};
-      int[] box2 = new int[] {0,0};
-      int[] box3 = new int[] {0,1};
-      int[] boxes = new int[] {1,1},{0,0},{0,1};*/
-      String input = "000111"
-
+      int tally = 0;
+      int[] input = new int[] {0,0,0,1,1,1,};
+      System.out.println("Original options: "+input);
+      System.out.println("0's represent silver, 1's represent gold.");
       System.out.println("User picks a box with 1 gold value.");
-      for (int i=0; i<input.Length; i++)
+      loop1:
+      for (int i : input)
       {
-         
+         if (i == 1)
+            input[i] -= i;
+         break loop1;
       }
+      System.out.println(input);
+      System.out.println("User grabs gold, what are the chances they grab gold again from the same box?");
+      System.out.println("We can eliminate the silver box option since we already grabbed a gold ball.");
+      System.out.println("This leaves us with "+input+" as options");
       
+      for (int i=0; i<100; i++)
+      {
+         Random rand = new Random();
+         int randomNum = rand.nextInt(3);
+         if (randomNum == 0)
+            tally += 0;
+         else 
+            tally += 1;
+      }
+      System.out.println("Tally after 100 games: "+tally);
    }
    
 }
