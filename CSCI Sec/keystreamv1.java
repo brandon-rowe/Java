5c170fd0b3e8f3d817dac570849c19e5fc2170bd
brandon-rowe <brandon@cyntax.org>
2019-03-05 11:17:41 -0500

    adding keystreamv1

--- CSCI Sec/keystreamv1.java
+++ CSCI Sec/keystreamv1.java
@@ -1,7 +1,10 @@
 // Lab 3
 // Computer Security
 // CSCI 455
-// AES Encryption
+// AES Encryption Seed
+// Create seed with currentTimeMillis()
+// Use seed to generate keystream of 128 bits
+// Char are ints from 0 to 9
 
 import java.util.*;
 import java.lang.*;
@@ -9,31 +12,30 @@ import java.lang.*;
 
 public class keystreamv1
 {
-   // Get the current Sys time used as seed for PRG
-   // Divide currentTimeMillis by 100 to get seconds
-   private long time = System.currentTimeMillis();
-   private long seed = time/1000; //Time in secs becomes seed
-   
-   public keystream(long seed)
-   {
-      this.seed = seed;
-   }
-
-   public double randomGenerator(seed)
-   {
-      Random generator = new Random(seed);
-      double num = generator.nextDouble()*(0.5);
-      return num;
-   }
-   
-   
 	public static void main(String[] args)
 	{
+      // Get the current Sys time used as seed for PRG
+      // Divide currentTimeMillis by 100 to get seconds
+      long time = System.currentTimeMillis();
+      long seed = time/1000; //Time in secs becomes seed
+      Random generator = new Random(seed);
+      String num = Double.toString(generator.nextDouble());
+      
 		System.out.println("Current Mills Time: " + time);
       System.out.println("Current Mills Time in seconds: " + seed);
-      double randomNum = randomGenerator(seed);
-      System.out.println("Your Pseudo Random Number is :" + randomNum);
-      
+      System.out.println("Your Pseudo Random Number is :" + num);  
 	}
-
+   
+   public String conversion(String str)
+   {
+      String[] num = str.split("");
+      for (int i=0; i<num.length(); i++)
+      {
+         if (num[i] == ".")
+         {
+            Random rand = new Random();
+            num[i] = rand.nextInt(10);
+         }
+      }
+   }
 }
