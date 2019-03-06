dbd4980a6cfa7560b22424ca094f0308c1e9d942
brandon-rowe <brandon@cyntax.org>
2019-03-05 19:51:02 -0500

    touch-ups

--- CSCI I, II, III/CSCI 3/Assignment1/prefixAvg1.java
+++ CSCI I, II, III/CSCI 3/Assignment1/prefixAvg1.java
@@ -9,12 +9,22 @@ public class prefixAvg1
    {
       //[2, 4, 6, 8, 10] [1, 3, 5, 7, 9] [3, 9, 6, 4, 1] 
       int[] X = new int[]{2, 4, 6, 8, 10};
+      int[] Y = new int[]{10, 8, 6, 4, 2};
       int[] A = new int[X.length];
       int tmp;
       
       for(int i=0; i<X.length; i++)
       {
          tmp = X[i];
+         A[i] = tmp/(i+1);
+      }
+      for (int i = 0; i<A.length; i++)
+      {
+         System.out.println(A[i]);
+      }
+      for(int i=0; i<Y.length; i++)
+      {
+         tmp = Y[i];
          A[i] = tmp/(i+1);
       }
       for (int i = 0; i<A.length; i++)
