a8236970cbfb08e302e4047625afc4a01c20b3bc
brandon-rowe <brandon@cyntax.org>
2019-07-09 09:40:17 -0400

    Assignment 1

---
+++ CSCI I, II, III/CSCI 3/Assignment1/GenericGeo.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class GenericGeo extends Progression<k>{
+   protected k r;
+   GenericGeo(){
+      this(1,1);
+   }
+   GenericGeo(k a, k base){
+      first = a;
+      r = base;
+   }
+   protected K nextValue(){
+      
+   }      
+}
