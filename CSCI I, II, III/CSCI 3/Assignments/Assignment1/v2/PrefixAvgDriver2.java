aee39e0da1d258f0a5c69a7cc753a6df9ba7a977
brandon-rowe <brandon@cyntax.org>
2019-07-18 14:04:50 -0400

    Update assignment1

--- CSCI I, II, III/CSCI 3/Assignments/Assignment1/v2/PrefixAvgDriver2.java
+++ CSCI I, II, III/CSCI 3/Assignments/Assignment1/v2/PrefixAvgDriver2.java
@@ -6,24 +6,28 @@ import java.lang.*;
 public class PrefixAvgDriver2{
 
    public static void main(String[] args){
-      int X [] = {2, 4, 6, 8, 10};
-      int Y [] = {1, 3, 5, 7, 9};
-      int Z [] = {3, 9, 6, 4, 1};
-      int Q [] = new int[5];
+      int n = 1000;
+      int X [] = new int[1000];
+      int Y [] = new int[5000];
+      int Z [] = new int[10000];
+      int Q [] = new int[n];
       
       PrefixAverage2 avg = new PrefixAverage2();
       Q = avg.testArray(X);
       
+      System.out.print("Start time in nanoseconds :  ");
       for(int i = 0; i<Q.length; i++){
          System.out.println(Q[i]);
       }   
       System.out.println();
-      System.out.print("Time in nanoseconds = ");
+      System.out.print("End time in nanoseconds = ");
       System.out.println(System.nanoTime());
       System.out.println();
       
+      n = Y.length;
       Q = avg.testArray(Y);
       
+      System.out.print("Start time in nanoseconds :  ");
       for(int i = 0; i<Q.length; i++){
          System.out.println(Q[i]);
       }    
@@ -32,8 +36,10 @@ public class PrefixAvgDriver2{
       System.out.println(System.nanoTime());
       System.out.println();
       
+      n= Z.length;
       Q = avg.testArray(Z);
       
+      System.out.print("Start time in nanoseconds :  ");
       for(int i = 0; i<Q.length; i++){
          System.out.println(Q[i]);
       }  
