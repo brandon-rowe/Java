05e781731180903bf381b1f27bfa6d7df258e0e7
brandon-rowe <brandon@cyntax.org>
2019-07-18 21:26:07 -0400

    testing for assignment 1

--- CSCI I, II, III/CSCI 3/Assignments/Assignment1/v2/test.java
+++ CSCI I, II, III/CSCI 3/Assignments/Assignment1/v2/test.java
@@ -5,33 +5,32 @@ import java.lang.*;
 public class test{
    public static void main(String[] args){
          int X [] = new int[100];
-         int n = X.length-1;
+         int n = X.length;
          int A [] = new int[n];
          Random rand = new Random();
          
          
-         for(int i=0; i<=n; i++){
+         for(int i=0; i<n; i++){
             X[i] = rand.nextInt(100);
          }
          
-         for(int i=0; i<=n; i++){
-            System.out.println(X[i]);
-         }
-         
-         
-      
-      
-         /*for(int k=0; k<in.length; k++){
-            X[k] = in[k];
-         }
-         
-         for(int i=0; i<X.length; i++){
+         for(int i=0; i<n; i++){
             int a = 0; 
             for(int j=0; j<=i; j++){ 
                a += X[j];
             }
             A[i] += (a/(i+1));
-         } */
+         }
+         
+         for(int i=0; i<n; i++){
+            System.out.println(X[i]);
+         }
+         System.out.println("End of random array.");
+         
+         for(int i=0; i<n; i++){
+            System.out.println(A[i]);
+         }
+         System.out.println("End of average array.");
       
   }
 
