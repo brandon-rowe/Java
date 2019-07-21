470f226554a1750e2228e20b0513cfd00c96ec8a
brandon-rowe <brandon@cyntax.org>
2019-07-21 14:42:10 -0400

    update test.java

--- CSCI I, II, III/CSCI 3/Assignments/Assignment1/v2/test.java
+++ CSCI I, II, III/CSCI 3/Assignments/Assignment1/v2/test.java
@@ -23,7 +23,6 @@ public class test{
          System.out.println("Creating average array for int A [].");
          System.out.print("Start time in nanoseconds = ");
          System.out.println(System.nanoTime());
-         System.out.println("Creating random array for int X [].");
          for(int i=0; i<n; i++){
             int a = 0; 
             for(int j=0; j<=i; j++){ 
@@ -38,14 +37,14 @@ public class test{
          
          System.out.println("Print random array.");
          for(int i=0; i<n; i++){
-            System.out.println(X[i]);
+            System.out.print(X[i] + ", ");
          }
          System.out.println("End of printing random array.");
          System.out.println();
         
          System.out.println("Print Avg array.");
          for(int i=0; i<n; i++){
-            System.out.println(A[i]);
+            System.out.print(A[i] + ", ");
          }
          System.out.println("End of printing Avg array.");
       
