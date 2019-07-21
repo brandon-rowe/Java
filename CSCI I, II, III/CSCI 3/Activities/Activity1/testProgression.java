a1ce2991e868eeea7063c0256c01e2560f629ac8
brandon-rowe <brandon@cyntax.org>
2019-07-21 18:03:45 -0400

    update CSCI 3 Activity 1

--- CSCI I, II, III/CSCI 3/Activities/Activity1/testProgression.java
+++ CSCI I, II, III/CSCI 3/Activities/Activity1/testProgression.java
@@ -4,8 +4,12 @@ import java.io.*;
 
 public class testProgression{
    public static void main(String[] args){
-      Progression pro = new Progression();
-      pro.printProgression(25);
+      Progression pro;
+      pro = new ArithProgression(1,2);
+      pro.printProgression(10);
+      pro = new GeomProgression(1,3);
+      pro.printProgression(10);
+      pro = new FiboProgression(3,4);
+      pro.printProgression(10);
    }
-
 }
