9a2083cb63a5b0372bfa0a55c4c513a69fc18bb8
brandon-rowe <brandon@cyntax.org>
2019-07-27 17:43:37 -0400

    Precious Metals Calculator

--- Independent/OZTcount/OZTcount.java
+++ Independent/OZTcount/OZTcount.java
@@ -4,16 +4,45 @@ import java.io.*;
 
 public class OZTcount{
    public static void main(String[] args){
-      float silverWeight;
-      float silverPrice;
-      float currentPrice;
+      float weight;
+      float price;
+      float currentPrice= 0.1f;
+      String metalType;
+      OZTcount ozt = new OZTcount();
       Scanner scan = new Scanner(System.in);
-      System.out.println("Please enter the weight of the coin in OZT: ");
-      silverWeight = scan.nextFloat();
-      System.out.println("Please enter the price of the coin per OZT: ");
-      silverPrice = scan.nextFloat();
-      currentPrice = silverWeight * silverPrice;
-      System.out.println("The current value of your silver is: $" + currentPrice);
+      
+      
+      System.out.println("What is the current weight of the metal in OZT: ");
+      weight = scan.nextFloat();
+      System.out.println("What is the current spot price of the metal per OZT: ");
+      price = scan.nextFloat();
+      
+      System.out.println("What is the type of the metal: ");
+      metalType = scan.nextLine();
+      if (metalType == "gold")
+            currentPrice = ozt.goldPrice(weight, price);
+      else if (metalType == "silver")
+            currentPrice = ozt.silverPrice(weight, price);
+      else if (metalType == "platinum")
+            currentPrice = ozt.platinumPrice(weight, price);
+      else  
+         {
+            System.out.println("Please enter a valid metal."); 
+            System.exit(0);
+         }
+      System.out.println("The current value of your " + metalType + " is: $" + currentPrice);
+   }
+   
+   public float goldPrice(float weight, float price){
+      return weight * price;
+   }
+   
+   public float silverPrice(float weight, float price){
+      return weight * price;
+   }
+   
+   public float platinumPrice(float weight, float price){
+      return weight * price;
    }
 
 }
