a9e620a76e37a1f0bbc681911e315ae9fca549dd
brandon-rowe <brandon@cyntax.org>
2019-07-29 23:22:19 -0400

    trouble commiting updated file

--- CSCI I, II, III/CSCI 3/javaSort.java
+++ CSCI I, II, III/CSCI 3/javaSort.java
@@ -6,7 +6,6 @@ public class javaSort{
    public static void main(String[] args){
          int X [] = new int[1000];
          int n = X.length;
-         int A [] = new int[n];
          Random rand = new Random();
          long startTime = 0;
          long endTime = 0;
@@ -62,6 +61,69 @@ public class javaSort{
          System.out.println();
          
          System.out.println("Sorting array for int Z [].");
+         startTime = System.nanoTime();
+         System.out.print("Start time in nanoseconds = ");
+         System.out.println(startTime);
+         Arrays.sort(Z);
+         endTime = System.nanoTime();
+         System.out.print("End time in nanoseconds = ");
+         System.out.println(endTime);
+         time = endTime - startTime;
+         System.out.print("Runtime in nanoseconds = ");
+         System.out.println(time);
+         System.out.println();
+         
+         int A [] = new int[50000];
+         n = A.length;
+         System.out.println("Creating random array for int A [50000].");
+         for(int i=0; i<n; i++){
+            A[i] = rand.nextInt(100);
+         }
+         System.out.println();
+         
+         System.out.println("Sorting array for int A [].");
+         startTime = System.nanoTime();
+         System.out.print("Start time in nanoseconds = ");
+         System.out.println(startTime);
+         Arrays.sort(A);
+         endTime = System.nanoTime();
+         System.out.print("End time in nanoseconds = ");
+         System.out.println(endTime);
+         time = endTime - startTime;
+         System.out.print("Runtime in nanoseconds = ");
+         System.out.println(time);
+         System.out.println();
+         
+         int B [] = new int[100000];
+         n = B.length;
+         System.out.println("Creating random array for int B [100000].");
+         for(int i=0; i<n; i++){
+            B[i] = rand.nextInt(100);
+         }
+         System.out.println();
+         
+         System.out.println("Sorting array for int B [].");
+         startTime = System.nanoTime();
+         System.out.print("Start time in nanoseconds = ");
+         System.out.println(startTime);
+         Arrays.sort(B);
+         endTime = System.nanoTime();
+         System.out.print("End time in nanoseconds = ");
+         System.out.println(endTime);
+         time = endTime - startTime;
+         System.out.print("Runtime in nanoseconds = ");
+         System.out.println(time);
+         System.out.println();
+         
+         int C [] = new int[1000000];
+         n = C.length;
+         System.out.println("Creating random array for int C [1000000].");
+         for(int i=0; i<n; i++){
+            C[i] = rand.nextInt(100);
+         }
+         System.out.println();
+         
+         System.out.println("Sorting array for int C [].");
          startTime = System.nanoTime();
          System.out.print("Start time in nanoseconds = ");
          System.out.println(startTime);
