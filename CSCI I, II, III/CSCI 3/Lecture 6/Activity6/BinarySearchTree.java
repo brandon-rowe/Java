a682ff00d2859155249750ef7407f2a32a2c7521
brandon-rowe <brandon@cyntax.org>
2019-08-05 21:52:39 -0400

    Finishing CSCI 3 Lecture 6 Activity6

--- CSCI I, II, III/CSCI 3/Lecture 6/Activity6/BinarySearchTree.java
+++ CSCI I, II, III/CSCI 3/Lecture 6/Activity6/BinarySearchTree.java
@@ -59,6 +59,22 @@ class BinarySearchTree {
 		} 
 	} 
 
+   Node lca(Node node, int n1, int n2)  
+    { 
+        if (node == null) 
+            return null; 
+   
+        // If both n1 and n2 are smaller than root, then LCA lies in left 
+        if (node.data > n1 && node.data > n2) 
+            return lca(node.left, n1, n2); 
+   
+        // If both n1 and n2 are greater than root, then LCA lies in right 
+        if (node.data < n1 && node.data < n2)  
+            return lca(node.right, n1, n2); 
+   
+        return node; 
+    } 
+
 	// Driver Program to test above functions 
 	public static void main(String[] args) { 
 		BinarySearchTree tree = new BinarySearchTree(); 
@@ -76,5 +92,22 @@ class BinarySearchTree {
 
 		// print inorder traversal of the BST 
 		tree.inorder(); 
+      
+      int n1 = 10, n2 = 14; 
+        Node t = tree.lca(tree.root, n1, n2); 
+        System.out.println("LCA of " + n1 + " and " + n2 + " is " + t.data); 
+   
+        n1 = 14; 
+        n2 = 8; 
+        t = tree.lca(tree.root, n1, n2); 
+        System.out.println("LCA of " + n1 + " and " + n2 + " is " + t.data); 
+   
+        n1 = 10; 
+        n2 = 22; 
+        t = tree.lca(tree.root, n1, n2); 
+        System.out.println("LCA of " + n1 + " and " + n2 + " is " + t.data); 
+   
 	} 
 } 
+
+
