0e53f3c458d9729e2fb4b95f16eca92de7ae8321
brandon-rowe <brandon@cyntax.org>
2019-08-06 19:04:16 -0400

    CSCI 3 Final Exam

---
+++ CSCI I, II, III/CSCI 3/scheduler.java
@@ -0,0 +1,59 @@
+public class scheduler implements Comparable<scheduler>
+{
+   public scheduler(int priority, int timeslice, String name)
+   {
+      if (priority >= -20 && priority <= 19)
+      {
+         this.priority = priority;
+      }
+      else
+         throw new Exception("Priority incorrect");
+      if (timeslice <= 100 && timeslice >= 1)
+      {
+         this .timeslice = timeslice;
+      }
+      else
+         throw new Exception("Length is incorrect.");
+         this.name = name;
+   }
+   private int priority;
+   private int timeslice;
+   private String name;
+   
+   public int getPriority()
+   {
+      return priority;
+   }
+   
+   public void setPriority(int priority) throws Exception
+   {
+      if (priority >= -20 && priority <= 19)
+         {this.priority = priority;}
+      else
+         throw new Exception("Priority incorrect.");
+   }
+   
+   public int getTimeSlice()
+   {
+      return timeslice;
+   }
+   
+   public void setTimeSlice(int timeslice) throws Exception
+   {
+      if (timeslice <= 100 && timeslice >= 1)
+         this.timeslice = timeslice;
+      else
+         throw new Exception("Length is incorrect.");
+   }
+   @Override
+   public int compareTo(Scheduler cpujob)
+   {
+      return (this.priority + cpujob.priority);
+   }
+   
+   public String getName()
+   {return name;}
+   public String getName()
+   {this.name = name;}
+   
+}
