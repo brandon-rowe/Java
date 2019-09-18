e0be6995c91b1542570f546a315f525541a5cf83
brandon-rowe <brandon@cyntax.org>
2019-09-17 20:53:50 -0400

    terrible assignment

--- CSCI 511/shell.java
+++ CSCI 511/shell.java
@@ -1,49 +1,39 @@
 import java.io.*;
 
 public class shell
-{
+{    
 
-    public class JavaStringHistory
-    {
-            private String[] history = new String[4];
-    }
-
-    	public static void main(String[] args) throws java.io.IOException 
+   public static void main(String[] args) throws java.io.IOException 
 	{
 
         	String commandLine;
         	BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 
-            	while (true) 
-		{
-            		//read the command
+         while (true) 
+		   {
             		System.out.print("browe shell>");
             		commandLine = console.readLine();
 
-            		//no input
             		if (commandLine.equals(""))
             		continue;
 
-            		//help
             		if (commandLine.equals("help"))
             		{
-               			System.out.println();
                 		System.out.println();
                 		System.out.println("Welcome to the shell");
-                		System.out.println("Written by: Lucky Rowe");
+                		System.out.println("Written by: B Rowe");
                 		System.out.println("CSCI 511 - Dr. Majundur");
                 		System.out.println();
-                		System.out.println("Commands to use:");
-                		System.out.println("1) cat prog.java");
+                		System.out.println("Commands:");
+                		System.out.println("1) clear");
                 		System.out.println("2) exit");
-                		System.out.println("3) clear");
-                		System.out.println();
+                		System.out.println("3) help");
                 		System.out.println();
             		}
 
             		if (commandLine.equals("clear"))
             		{
-               			for (int c=0; c<100; c++)
+               			for (int c=0; c<50; c++)
                			{
                   			System.out.println();
                			}
@@ -53,36 +43,10 @@ public class shell
             		{
                 		System.exit(0);
             		}
-			else
-			{
-				System.out.println("Incorrect Command");
-			}
+			
         	}
     	}
 }
 
 
-			/*if (commandLine.endsWith(".java"))
-            		{
-              			if(commandLine.startsWith("cat"))
-              			{
-                			System.out.println("test");
-                			ProcessBuilder pb = new ProcessBuilder();
-                			//pb = new ProcessBuilder(commandLine);
-              			}
-              			else
-              			{
-                  			System.out.println("Incorrect Command");
-              			}
-            		}
-			//history
-            		//if(commandLine.equals('*'))
-            		//{
-              			//new class HistoryStringArray();
-             			// {
-               			//  history[4] = history[3]
-                		//  history[3] = history[2]
-                		//  history[2] = history[1]
-                		//  history[1] = history[0]
-                		//  history[0] = commandLine
-                		//}*/
+			
